bdd9d0c991bdcabc85485db88106a86d227e3c0b
RamShekade <127588613+RamShekade@users.noreply.github.com>
2025-07-29 20:41:40 +0530

    Update CompanyLogin.jsx

--- src/company/Login/CompanyLogin.jsx
+++ src/company/Login/CompanyLogin.jsx
@@ -24,7 +24,7 @@ const CompanyLogin = () => {
     setLoading(true);
 
     try {
-      const res = await fetch('https://placement.suyahsawant.com/api/api/company-auth/login', {
+      const res = await fetch('api/company-auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
